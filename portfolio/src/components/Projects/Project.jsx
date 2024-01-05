ab52e524175aef4d9e6c0bb18345f7310d1cceaf
Khang <thien-khang.cao@epitech.eu>
2024-01-05 15:04:14 +0100

    Fix : display project's description in responsive way (need improvement)

--- portfolio/src/components/Projects/Project.jsx
+++ portfolio/src/components/Projects/Project.jsx
@@ -9,7 +9,7 @@ const Project = (props) => {
         <div className='disc'>
             <h1>Description</h1>
             <p>{disc}
-            <a href='#'>Demo</a>
+                <a href='#'>Demo</a>
             </p>
 
         </div>
@@ -20,49 +20,56 @@ const Project = (props) => {
 export default Project;
 
 const Container = styled.div`
-    height: 10rem;
-    background-color: #4e5156;
-    margin: 0 0.5rem;
-    padding: 0 0.5rem;
-    border-radius: 5px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        transition: transform 400ms ease-in-out;
+  /* styles for larger screens */
+  height: 10rem;
+  background-color: #4e5156;
+  margin: 0 0.5rem;
+  padding: 0 0.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: transform 400ms ease-in-out;
+  }
+  .disc {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: -10rem;
+    text-align: left;
+    padding: 0.5rem;
+    background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8));
+    transition: all 400ms ease-in-out;
+    h1 {
+      font-size: 1rem;
     }
-    .disc{
-        position: absolute;
-        right: 0;
-        left: 0;
-        bottom: -10rem;
-        text-align: left;
-        padding: 0.5rem;
-        background: linear-gradient(rgba(0,0,0, 0.100), rgba(0,0,0, 0.80));
-        transition: all 400ms ease-in-out;
-        h1{
-            font-size: 1rem;
-        }
-    
-        p{
-            width: 90%;
-            font-size: 0.8rem;
-            a{
-                margin-left: 0.4rem;
-                color: red;
-            }
-        }
+    p {
+      width: 90%;
+      font-size: 0.8rem;
+      a {
+        margin-left: 0.4rem;
+        color: red;
+      }
     }
+  }
+  
+  /* styles for smaller screens (mobile) */
+  @media (max-width: 768px) {
+    height: auto; /* Adjust height as needed for mobile layout */
+    .disc {
+      bottom: 0;
+    }
+  }
 
-    &:hover > img{
-        transform: scale(1.3);
-    }
+  &:hover > img {
+    transform: scale(1.3);
+  }
 
-    &:hover > .disc{
-        bottom: 0;
-    }
-
+  &:hover > .disc {
+    bottom: 0;
+  }
 `;
